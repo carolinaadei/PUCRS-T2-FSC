90f20f7424f28eea3e4ba541c833bfc1b10fe69c
Carolina de Souza Gonçalves <cahdesouzag@gmail.com>
2025-10-15 16:11:32 -0300

    ajustes iniciais

--- palindromo.s
+++ palindromo.s
@@ -56,12 +56,12 @@ main:
     li a2, 38           # a2 = right
     jal ra, is_palindromo
 
-    # Insira aqui o loop principal, ofereca ao usuario se ele deseja continuar ou não
-    # 
+    # loop principal, ofereca ao usuario se ele deseja continuar ou não
+    
 
     # Encerrar
     // Encerramento coloca o 10 no registrador a7 e o ecall executa a chamada do a7
-    li a7, 10          # ecall: exit
+    li a7, 10          
     ecall
 
 # ------------------------------------------------------------
@@ -75,19 +75,19 @@ main:
 # ------------------------------------------------------------
 is_palindromo:
     // O stack pointer desce 4 bytes a pilha e salva o endereço para retorno
-    addi sp, sp, -4
+    addi sp, sp, -16
     // O sw salva o valor na memória e salva o vetor ra, registrador de retorno, na pilha
-    sw   ra, 0(sp)
+    sw ra, 12(sp)
+    sw s0, 8(sp)
+    sw s1, 4(sp)
+    sw s2, 0(sp)
 
-    # TODO: implementar logica recursiva conforme enunciado
-    # Retorno provisrio:
-    // 
-    li   a0, 0
+    #logica recursiva 
+    mv s0, a0
+    mv s1, a1
+    mv s2, a2
 
-    //Restaura o endereço de retorno da pilha
-    lw   ra, 0(sp)
-    // Libera espaço reservado na pilha
-    addi sp, sp, 4
-    // o jr, jump register volta para a função de chamada com o endereço ra
-    jr ra
+
+    # Retorno:
+    
 
